a4739eb525a09adc854d6ac3863d609e93ac4ac4
darkside <andrey.garbuz@gmail.com>
2013-01-18 00:01:09 +0200

    Add "layout" controller and route for it "main" action. Add application layout and sample action for testing slim works fine

---
+++ app/controllers/layout_controller.rb
@@ -0,0 +1,5 @@
+class LayoutController < ApplicationController
+  def main
+    render 'main'
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Bbsmile::Application.routes.draw do
+
+  match 'main' => 'layout#main'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
