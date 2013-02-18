51e680cb6b18df695e0fa9dece8f65fd4ffb4321
darkside <andrey.garbuz@gmail.com>
2013-02-19 00:45:25 +0200

    Add default stylesheet automaticaly depending on layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,23 @@ module ApplicationHelper
   def title(page_title)
     content_for(:title) { page_title }
   end
+
+  def default_stylesheet
+    case current_layout
+    when 'layout_main'
+      'main'
+    when 'layout_inner'
+      'inner'
+    end
+  end
+
+  private
+    def current_layout
+      layout = controller.send(:_layout)
+      if layout.instance_of? String
+        layout
+      else
+        File.basename(layout.identifier).split('.').first
+      end
+    end
 end
