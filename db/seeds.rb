29741e7c702bf92c2ef0e13da21cf2581257b3eb
darkside <andrey.garbuz@gmail.com>
2013-02-20 23:39:29 +0200

    Remove db seed data. Add view for information page

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-pages = Page.create [
-  {title: 'Информация', url: 'information'}
-]
+# pages = Page.create [
+#   {title: 'Информация', url: 'information'}
+# ]
