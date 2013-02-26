2c778c84c3699a9d6029b2f9f156b7b93f1a51a9
darkside <andrey.garbuz@gmail.com>
2013-02-26 12:09:04 +0200

    Add category factory

--- spec/factories/pages.rb
+++ spec/factories/pages.rb
@@ -1,3 +1,19 @@
 FactoryGirl.define do
+  sequence(:random_url) { Faker::Lorem.word }
+
   factory :page
+
+  factory :category do
+    ignore do
+      subcategory Faker::Name.title
+    end
+    url { generate(:random_url) }
+    after(:create) do |category, evaluator|
+      create(:subcategory, title: evaluator.subcategory, parent: category)
+    end
+  end
+
+  factory :subcategory, class: Category do
+    url { generate(:random_url) }
+  end
 end
