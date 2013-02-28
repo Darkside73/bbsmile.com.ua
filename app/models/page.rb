c2d42ff3a6f4ee6159105f91b51bc3f9b248a22a
darkside <andrey.garbuz@gmail.com>
2013-02-28 19:35:30 +0200

    Db seeds for category structure

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,4 +1,4 @@
 class Page < ActiveRecord::Base
-  attr_accessible :title, :url, :parent
+  attr_accessible :title, :url, :parent, :type
   has_ancestry
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,10 +7,30 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-pages = Page.create [
+Page.create [
   {title: 'Доставка', url: 'information/delivery'},
   {title: 'Оплата', url: 'information/payment'},
   {title: 'Гарантии', url: 'information/warranty'}
 ]
-category = Category.create({title: 'Детские коляски', url: 'detskie-kolyaski'})
-Category.create({title: 'Прогулочные', url: 'detskie-kolyaski/progulochnye', parent: category})
+Category.create({title: 'Детские коляски', url: 'detskie-kolyaski'}).children.create [
+  {title: 'Прогулочные', url: 'detskie-kolyaski/progulochnye', type: 'Category'},
+  {title: 'Трансформеры', url: 'detskie-kolyaski/transformery', type: 'Category'},
+  {title: 'Аксессуары', url: 'detskie-kolyaski/accessories', type: 'Category'},
+  {title: 'Коляски 2 в 1', url: 'detskie-kolyaski/2in1', type: 'Category'},
+  {title: 'Коляски 3 в 1', url: 'detskie-kolyaski/3in1', type: 'Category'}
+]
+Category.create({title: 'Автокресла', url: 'avtokresla'}).children.create [
+  {title: 'Бустеры', url: 'avtokresla/bustery', type: 'Category'},
+  {title: 'Аксессуары', url: 'avtokresla/accessories', type: 'Category'}
+]
+Category.create({title: 'Детская комната', url: 'detskaya-komnata'}).children.create [
+  {title: 'Кроватки', url: 'detskaya-komnata/krovatki', type: 'Category'},
+  {title: 'Матрасы', url: 'detskaya-komnata/matrasy', type: 'Category'},
+  {title: 'Шкафы', url: 'detskaya-komnata/shkafy', type: 'Category'},
+  {title: 'Столы, стулья', url: 'detskaya-komnata/stoly-stulya', type: 'Category'}
+]
+Category.create({title: 'Электроприборы', url: 'elektropribory'}).children.create [
+  {title: 'Радионяни, видеоняни', url: 'elektropribory/radionyani', type: 'Category'},
+  {title: 'Весы и термометры', url: 'elektropribory/vesy', type: 'Category'},
+  {title: 'Увлажнители воздуха', url: 'elektropribory/uvlazhniteli', type: 'Category'}
+]
