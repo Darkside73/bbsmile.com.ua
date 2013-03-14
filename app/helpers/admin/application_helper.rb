f01227964b91afd473c4951c6b6841aaf1e0906a
darkside <andrey.garbuz@gmail.com>
2013-03-14 23:21:05 +0200

    Extract "boxed" partial layout for admin categories forms

--- app/helpers/admin/application_helper.rb
+++ app/helpers/admin/application_helper.rb
@@ -1,5 +1,5 @@
 module Admin::ApplicationHelper
   def render_form_for_category(category = nil, url= nil)
-    render 'form', category: category.blank? ? @category : category, url: url
+    render partial: 'form', locals: { category: category.blank? ? @category : category, url: url }
   end
 end
