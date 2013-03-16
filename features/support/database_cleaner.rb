216d85e5eb476cc6429ff26e2777403675fd7e51
darkside <andrey.garbuz@gmail.com>
2013-03-16 17:44:11 +0200

    Clean database before all features start (database is dirty after rspec despite transactional mode on)

--- features/support/database_cleaner.rb
+++ features/support/database_cleaner.rb
@@ -3,6 +3,7 @@ begin
   require 'database_cleaner/cucumber'
 
   DatabaseCleaner.strategy = :truncation
+  DatabaseCleaner.clean
 rescue NameError
   raise "You need to add database_cleaner to your Gemfile (in the :test group) if you wish to use it."
 end
