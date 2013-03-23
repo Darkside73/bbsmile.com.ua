dd19ee6d93092e2ebaf6fa9e999801cd42c97c5a
darkside <andrey.garbuz@gmail.com>
2013-03-23 02:56:30 +0200

    Highlight categories item in admin menu

--- app/helpers/admin/application_helper.rb
+++ app/helpers/admin/application_helper.rb
@@ -1,4 +1,17 @@
 module Admin::ApplicationHelper
+
+  def controller?(*controller)
+    controller.include?(params[:controller])
+  end
+
+  def action?(*action)
+    action.include?(params[:action])
+  end
+
+  def unless_active_attr(page)
+    controller?(page) ? {class: :active} : {}
+  end
+
   def render_form_for_category(category = nil, url= nil)
     render partial: 'form', locals: { category: category.blank? ? @category : category, url: url }
   end
