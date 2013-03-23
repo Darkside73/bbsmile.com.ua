c242bc90cd43d2ae060cd3c7b8cbccd1241d3dcf
darkside <andrey.garbuz@gmail.com>
2013-03-23 19:33:53 +0200

    Comment out HTTP base authentication for admin namespace

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -1,5 +1,5 @@
 class Admin::ApplicationController < ApplicationController
   layout 'admin/layouts/application'
 
-  # http_basic_authenticate_with(name: 'admin', password: 'Y5petRup') if Rails.env == 'production'
+  http_basic_authenticate_with(name: 'admin', password: 'Y5petRup') if Rails.env == 'production'
 end
