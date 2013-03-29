932c8719d36e1ac10df8b65420ad1dfffdbedd54
darkside <andrey.garbuz@gmail.com>
2013-03-30 01:29:07 +0200

    Change category model:
    - delegate title and url to page model;
    - override "arrange" singleton method
      to fix ancestry default order bug

--- app/models/category.rb
+++ app/models/category.rb
@@ -5,12 +5,21 @@ class Category < ActiveRecord::Base
   has_one :page, as: :pageable
   accepts_nested_attributes_for :page
 
+  delegate :title, :url, to: :page
+
   has_ancestry
   acts_as_list scope: [:ancestry]
   default_scope order: :position
 
   before_save do |category|
     raise ActiveRecord::ActiveRecordError if category.parent && category.parent.leaf
+  end
+
+  class << self
+    alias_method :ancestry_arrange, :arrange
+    def arrange
+      self.unscoped.ancestry_arrange(order: :position)
+    end
   end
 
 end
