6ff44552e823e4df74ad2d31b59819b44f3f5406
darkside <andrey.garbuz@gmail.com>
2013-04-15 23:03:43 +0300

    Provide options for new cookie-base session store mechanism

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Bbsmile::Application.config.secret_token = '7d61a6d635940e2ba07cb359407f7d8779e640d800aef21515e7a9dd019f8f7ed0ff3a1893265cac7682a5926a9718078f56f74c18f46d120e6f24a5e82daa7e'
+Bbsmile::Application.config.secret_key_base = '7d61a6d635940e2ba07cb359407f7d8779e640d800aef21515e7a9dd019f8f7ed0ff3a1893265cac7682a5926a9718078f56f74c18f46d120e6f24a5e82daa7e'
