9372837a93d155372d90073386727cf1bdb30ec7
darkside <andrey.garbuz@gmail.com>
2013-04-16 22:26:25 +0300

    Add secret token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,5 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
+Bbsmile::Application.config.secret_token = '14dcaafd620edabbd60159eacc1e6e2a4a56619c1418e0b047d90d287d52abd56e3fc34a27220aeae44026610e73cb04d74ce9eba4428b155321e91f5c1740f3'
 Bbsmile::Application.config.secret_key_base = '7d61a6d635940e2ba07cb359407f7d8779e640d800aef21515e7a9dd019f8f7ed0ff3a1893265cac7682a5926a9718078f56f74c18f46d120e6f24a5e82daa7e'
