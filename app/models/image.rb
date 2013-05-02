6993a0ea0fdfcad43a15c56738d6a362bec06c41
darkside <andrey.garbuz@gmail.com>
2013-05-02 21:25:57 +0300

    Add paperclip styles for images

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,7 +1,7 @@
 class Image < ActiveRecord::Base
   belongs_to :imageable, polymorphic: true
 
-  has_attached_file :asset
+  has_attached_file :asset, styles: { thumb: '98x112#', medium: '444' }
   validates_attachment :asset, presence: true,
                        # content_type: { content_type: ['image/jpg', 'image/png', 'image/gif'] },
                        size: { in: 0..1.megabytes }
