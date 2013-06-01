1a1904ce8ae646381ade65828f00795d7522f9cf
darkside <andrey.garbuz@gmail.com>
2013-06-01 23:18:39 +0300

    Update categories_map.yml and use strip for category name from source

--- lib/converter/products_importer.rb
+++ lib/converter/products_importer.rb
@@ -49,7 +49,7 @@ module Converter
       results.all = @csv.count
       @csv.each_with_index do |row, index|
         source = row.to_hash
-        category_title = @categories_map[source['category']]
+        category_title = @categories_map[source['category'].strip]
         unless category_title.blank?
           begin
             product = create_product_from_source(source, category_title)
