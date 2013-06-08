a95fff94921a620ddd88726fb32ce1302cb72930
darkside <andrey.garbuz@gmail.com>
2013-06-09 01:50:04 +0300

    Restrict access to whole application with basic HTTP auth

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -1,8 +1,6 @@
 class Admin::ApplicationController < ApplicationController
   layout 'admin/layouts/application'
 
-  http_basic_authenticate_with(name: 'admin', password: 'Y5petRup') if Rails.env == 'production'
-
   def flashes_in_json
     { error: flash[:error], flash: render_to_string(partial: 'flashes') }
   end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   layout "layout_inner"
 
+  http_basic_authenticate_with(name: 'admin', password: 'Y5petRup') if Rails.env == 'production'
+
   protected
     def pageable_from_slug
       Page.visible.find_by!(url: params[:slug]).pageable
