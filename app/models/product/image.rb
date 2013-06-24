27013ea57087df94f55f9b8c933be1fe93ba2732
darkside <andrey.garbuz@gmail.com>
2013-06-24 20:20:44 +0300

    Change convert options for product and variant image thumbs styles

--- app/models/product/image.rb
+++ app/models/product/image.rb
@@ -1,7 +1,10 @@
 class Product::Image < Asset
   has_attached_file :attachment,
-                    styles: { thumb: '98x112#', medium: '444>', grid: '300x200' },
-                    convert_options: { grid: '-background white -gravity Center -extent 300x200' },
+                    styles: { thumb: '98x112', medium: '444>', grid: '300x200' },
+                    convert_options: {
+                      grid: '-background white -gravity Center -extent 300x200',
+                      thumb: '-background white -gravity Center -extent 98x112'
+                    },
                     url: DEFAULT_URL, path: DEFAULT_PATH
   validates_attachment_content_type :attachment, content_type: /^image\/(png|gif|jpg|jpeg)/,
                                     message: I18n.t('errors.messages.paperclip.content_type_image')
