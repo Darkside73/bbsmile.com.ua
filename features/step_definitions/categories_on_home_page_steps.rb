bbd9690e27e739b27968f6c3215f9a188cae4db6
darkside <andrey.garbuz@gmail.com>
2013-07-02 23:10:43 +0300

    Fix cucumber steps due to CSS id was changed

--- features/step_definitions/categories_on_home_page_steps.rb
+++ features/step_definitions/categories_on_home_page_steps.rb
@@ -3,11 +3,11 @@ Given /^Product category "(.*?)" and subcategory "(.*?)"$/ do |category, subcate
 end
 
 Then /^I should see link "(.*?)" as category$/ do |text|
-  find_by_id('catalog-items').find_link text
+  find_by_id('whole-catalog-items').find_link text
 end
 
 Then /^I should see link "(.*?)" as it subcategory$/ do |text|
-  find_by_id('catalog-items').find_link text
+  find_by_id('whole-catalog-items').find_link text
 end
 
 Given(/^Product category "(.*?)" and subcategories "(.*?)", "(.*?)", "(.*?)"$/) do |category, *subcategories|
