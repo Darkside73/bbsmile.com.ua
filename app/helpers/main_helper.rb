14c0a2a2fd67cf154d87c0d834e1851f89f69bcb
darkside <andrey.garbuz@gmail.com>
2013-07-31 19:17:52 +0300

    Fix landing item helper

--- app/helpers/main_helper.rb
+++ app/helpers/main_helper.rb
@@ -1,7 +1,7 @@
 module MainHelper
   def landing_item_for(items, options, &block)
     options[:items] = items
-    default_block = proc { render items }
+    default_block = proc { |items| render items }
     render({ layout: 'landing_item', locals: options }, &block || default_block)
   end
 end
