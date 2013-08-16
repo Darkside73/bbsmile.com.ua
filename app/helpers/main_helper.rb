d0db30293857017fbea1dd3fcd137e7d845bf6be
darkside <andrey.garbuz@gmail.com>
2013-08-16 21:05:41 +0300

    Add more promo to main page

--- app/helpers/main_helper.rb
+++ app/helpers/main_helper.rb
@@ -5,12 +5,20 @@ module MainHelper
     render({ layout: 'landing_item', locals: options }, &block || default_block)
   end
 
-  def luvion_promo_items
-    slugs = [
-      'videonyanya-luvion-supreme-robot', 'videonyanya-luvion-grand-elite',
-      'videonyanya-luvion-prestige-touch', 'mnogotsvetnyiy-nochnichok-luvion-ecoled'
-    ]
+  def luvion_promo_items(promo_id)
+    slugs = {
+      night_light: [
+        'videonyanya-luvion-supreme-robot', 'videonyanya-luvion-grand-elite',
+        'videonyanya-luvion-prestige-touch', 'mnogotsvetnyiy-nochnichok-luvion-ecoled'
+      ],
+      thermometer: [
+        'tsifrovoy-termometr-luvion-bath-and-room',
+        'radionyanya-tomy-classic-ta-100', 'radionyanya-tomy-digital-td-300',
+        'radionyanya-tomy-digital-plus-td-350', 'radionyanya-tomy-digital-tf-525',
+        'radionyanya-tomy-digital-tf-550', 'radionyanya-tomy-digital-tf-550'
+      ]
+    }
 
-    slugs.map { |slug| slug = Page.find_by(url: slug) }.reject(&:nil?)
+    slugs[promo_id].map { |slug| slug = Page.find_by(url: slug) }.reject(&:nil?)
   end
 end
