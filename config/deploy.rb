24e90a1e71128e46db01295f1acdc2c705befab2
darkside <andrey.garbuz@gmail.com>
2013-09-07 23:10:29 +0300

    Use multistage capistrano extension

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,104 +1,45 @@
-# По умолчанию для дистрибуции проектов используется Bundler.
-# Эта строка включает автоматическое обновление и установку
-# недостающих gems, указанных в вашем Gemfile.
-#
-## !!! Не забудьте добавить
-# gem 'capistrano'
-# gem 'unicorn'
-#
-# в ваш Gemfile.
-#
-# Если вы используете другую систему управления зависимостями,
-# закомментируйте эту строку.
+set :stages, %w(production beta)
+set :default_stage, "production"
+require 'capistrano/ext/multistage'
+
 require 'bundler/capistrano'
 
-
-## Чтобы не хранить database.yml в системе контроля версий, поместите
-## dayabase.yml в shared-каталог проекта на сервере и раскомментируйте
-## следующие строки.
-
-# after "deploy:update_code", :copy_database_config
-# task :copy_database_config, roles => :app do
-#   db_config = "#{shared_path}/database.yml"
-#   run "cp #{db_config} #{release_path}/config/database.yml"
-# end
-
-# В rails 3 по умолчанию включена функция assets pipelining,
-# которая позволяет значительно уменьшить размер статических
-# файлов css и js.
-# Эта строка автоматически запускает процесс подготовки
-# сжатых файлов статики при деплое.
-# Если вы не используете assets pipelining в своем проекте,
-# или у вас старая версия rails, закомментируйте эту строку.
-load 'deploy/assets'
-
-# Для удобства работы мы рекомендуем вам настроить авторизацию
-# SSH по ключу. При работе capistrano будет использоваться
-# ssh-agent, который предоставляет возможность пробрасывать
-# авторизацию на другие хосты.
-# Если вы не используете авторизацию SSH по ключам И ssh-agent,
-# закомментируйте эту опцию.
 ssh_options[:forward_agent] = true
 
-# Имя вашего проекта в панели управления.
-# Не меняйте это значение без необходимости, оно используется дальше.
-set :application,     "bbsmile"
-
-# Сервер размещения проекта.
+set :application,     'bbsmile'
 set :deploy_server,   "fluorine.locum.ru"
-
-# Не включать в поставку разработческие инструменты и пакеты тестирования.
 set :bundle_without,  [:development, :test]
-
 set :user,            "hosting_darkside"
 set :login,           "darkside"
 set :use_sudo,        false
-set :deploy_to,       "/home/#{user}/projects/#{application}"
-set :unicorn_conf,    "/etc/unicorn/#{application}.#{login}.rb"
-set :unicorn_pid,     "/var/run/unicorn/#{application}.#{login}.pid"
-set :bundle_dir,      File.join(fetch(:shared_path), 'gems')
-set :rails_env,       "production"
+set(:deploy_to)       { "/home/#{user}/projects/#{application_dir}" }
+set(:unicorn_conf)    { "/etc/unicorn/#{application_dir}.#{login}.rb" }
+set(:unicorn_pid)     { "/var/run/unicorn/#{application_dir}.#{login}.pid" }
+set(:unicorn_start_cmd) { "(cd #{deploy_to}/current; rvm use #{rvm_ruby_string} do bundle exec unicorn_rails -Dc #{unicorn_conf})" }
+set(:bundle_dir)      { File.join(fetch(:shared_path), 'gems') }
 role :web,            deploy_server
 role :app,            deploy_server
-role :db,             deploy_server, :primary => true
+role :db,             deploy_server, primary: true
 
-
-# Следующие строки необходимы, т.к. ваш проект использует rvm.
 set :rvm_ruby_string, "2.0.0"
-set :rake,            "RAILS_ENV=#{rails_env} rvm use #{rvm_ruby_string} do rake"
-set :bundle_cmd,      "RAILS_ENV=#{rails_env} rvm use #{rvm_ruby_string} do bundle"
+set(:rake)            { "RAILS_ENV=#{rails_env} rvm use #{rvm_ruby_string} do rake" }
+set(:bundle_cmd)      { "RAILS_ENV=#{rails_env} rvm use #{rvm_ruby_string} do bundle" }
 
-
-# Настройка системы контроля версий и репозитария,
-# по умолчанию - git, если используется иная система версий,
-# нужно изменить значение scm.
 set :scm,             :git
-
-# Предполагается, что вы размещаете репозиторий Git в вашем
-# домашнем каталоге в подкаталоге git/<имя проекта>.git.
-# Подробнее о создании репозитория читайте в нашем блоге
-# http://locum.ru/blog/hosting/git-on-locum
-# set :repository,      "ssh://#{user}@#{deploy_server}/home/#{user}/git/#{application}.git"
-
-## Если ваш репозиторий в GitHub, используйте такую конфигурацию
-set :repository,    "ssh://git@bitbucket.org/darkside73/rails.bbsmile.com.ua.git"
+set :repository,      "ssh://git@bitbucket.org/darkside73/rails.bbsmile.com.ua.git"
 set :git_enable_submodules, 1
 
 set :shared_children, shared_children + %w{public/uploads}
 
-## --- Ниже этого места ничего менять скорее всего не нужно ---
-
 before 'deploy:finalize_update', 'set_current_release'
-task :set_current_release, :roles => :app do
-    set :current_release, latest_release
+task :set_current_release, roles: :app do
+  set :current_release, latest_release
 end
 
+task :show_var do
+  puts shared_path
+end
 
-set :unicorn_start_cmd, "(cd #{deploy_to}/current; rvm use #{rvm_ruby_string} do bundle exec unicorn_rails -Dc #{unicorn_conf})"
-
-
-
-# - for unicorn - #
 namespace :deploy do
   desc "Start application"
   task :start, :roles => :app do
@@ -115,36 +56,3 @@ namespace :deploy do
     run "[ -f #{unicorn_pid} ] && kill -USR2 `cat #{unicorn_pid}` || #{unicorn_start_cmd}"
   end
 end
-
-namespace :sitemap do
-  task :copy_old do
-    run <<-EOF
-      if [ -e #{previous_release}/public/sitemap.xml.gz ]; then
-        cp #{previous_release}/public/sitemap* #{current_release}/public/;
-      else
-        echo "No sitemap found. Try to refresh";
-      fi
-    EOF
-  end
-  task :refresh do
-    run "cd #{latest_release} && #{rake} sitemap:refresh"
-  end
-end
-
-namespace :backup do
-  desc "Backup the database"
-  task :db, :roles => :db do
-    run "mkdir -p #{deploy_to}/backups"
-    run "cd #{deploy_to}; pg_dump -U darkside_bbsmi52 darkside_bbsmi52 -h postgresql5.locum.ru -f backups/#{Time.now.utc.strftime('%Y%m%d%H%M%S')}.sql"
-  end
-end
-
-# use delayed_job recipes
-require 'delayed/recipes'
-set :delayed_job_command, "rvm use #{rvm_ruby_string} do bin/delayed_job"
-
-after "deploy:stop",    "delayed_job:stop"
-after "deploy:start",   "delayed_job:start"
-after "deploy:restart", "delayed_job:restart"
-
-after "deploy:update_code", "sitemap:copy_old"
---
+++ config/deploy/production.rb
@@ -0,0 +1,37 @@
+load 'deploy/assets'
+
+set :application_dir, "bbsmile"
+
+set :rails_env, "production"
+
+namespace :sitemap do
+  task :copy_old do
+    run <<-EOF
+      if [ -e #{previous_release}/public/sitemap.xml.gz ]; then
+        cp #{previous_release}/public/sitemap* #{current_release}/public/;
+      else
+        echo "No sitemap found. Try to refresh";
+      fi
+    EOF
+  end
+  task :refresh do
+    run "cd #{latest_release} && #{rake} sitemap:refresh"
+  end
+end
+
+namespace :backup do
+  desc "Backup the database"
+  task :db, :roles => :db do
+    run "mkdir -p #{deploy_to}/backups"
+    run "cd #{deploy_to}; pg_dump -U darkside_bbsmi52 darkside_bbsmi52 -h postgresql5.locum.ru -f backups/#{Time.now.utc.strftime('%Y%m%d%H%M%S')}.sql"
+  end
+end
+
+require 'delayed/recipes'
+set :delayed_job_command, "rvm use #{rvm_ruby_string} do bin/delayed_job"
+
+after "deploy:stop",    "delayed_job:stop"
+after "deploy:start",   "delayed_job:start"
+after "deploy:restart", "delayed_job:restart"
+
+after "deploy:update_code", "sitemap:copy_old"
