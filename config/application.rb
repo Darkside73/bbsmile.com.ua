5b013e38ea0bc37366ce9423a25e6ce19cac0cb6
darkside <andrey.garbuz@gmail.com>
2013-12-04 23:45:26 +0200

    Avoid I18N deprecation message

--- config/application.rb
+++ config/application.rb
@@ -33,6 +33,8 @@ module Bbsmile
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :ru
 
+    config.i18n.enforce_available_locales = true
+
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
 
