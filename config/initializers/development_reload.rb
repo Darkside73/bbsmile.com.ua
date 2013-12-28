8eb8179a6613814c6f82e13a70652b3092489ec6
darkside <andrey.garbuz@gmail.com>
2013-12-28 19:45:36 +0200

    Revert "Add development_reload initializer for prevent" due to incomatibility with Rails 4.1 (see https://github.com/rails/rails/pull/10652 for more details)
    This reverts commit 071304583265257da997e56493d59722f1accfe4.

--- config/initializers/development_reload.rb
+++
@@ -1,8 +0,0 @@
-if Rails.env.development?
-  ActionDispatch::Callbacks.after do
-    Dir.glob(["#{Rails.root}/app/models/*/*.rb", "#{Rails.root}/app/mailers/*/*.rb", "#{Rails.root}/lib/**/*.rb"]) do |entry|
-      # require_dependency entry
-      load entry
-    end
-  end
-end
