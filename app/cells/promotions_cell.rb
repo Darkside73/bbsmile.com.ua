893058e881f2f6e8661b0037f0f41c33c7368ddd
darkside <andrey.garbuz@gmail.com>
2014-01-28 16:39:41 +0200

    Remove two promos on category page

--- app/cells/promotions_cell.rb
+++ app/cells/promotions_cell.rb
@@ -8,18 +8,6 @@ class PromotionsCell < Cell::Rails
         anchor: 'promo4', utm_campaign: 'veres-sonya-matras',
         utm_medium: 'category_page_side',
         utm_source: 'local'
-      ),
-      '2.jpg' => page_path(
-        'krovatka-veres-sonya-ld-11-s-yaschikom',
-        utm_campaign: 'veres-sonya-ld-11',
-        utm_medium: 'category_page_side',
-        utm_source: 'local'
-      ),
-      '3.jpg' => page_path(
-        'krovatka-veres-sonya-ld-7-s-yaschikom',
-        utm_campaign: 'veres-sonya-ld-7',
-        utm_medium: 'category_page_side',
-        utm_source: 'local'
       )
     }
     banners = banners.to_a.shuffle
