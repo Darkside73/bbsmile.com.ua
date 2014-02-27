320eda8613f0533b89cef6fcef5d849192421bcf
darkside <andrey.garbuz@gmail.com>
2014-02-27 12:34:47 +0200

    Expire promotions

--- app/cells/promotions_cell.rb
+++ app/cells/promotions_cell.rb
@@ -2,14 +2,7 @@ class PromotionsCell < Cell::Rails
 
   def category(args)
     category = args[:category]
-    banners = {
-      '1.jpg' => page_path(
-        'promotions',
-        anchor: 'promo4', utm_campaign: 'veres-sonya-matras',
-        utm_medium: 'category_page_side',
-        utm_source: 'local'
-      )
-    }
+    banners = {}
     banners = banners.to_a.shuffle
     @banners = if category.id == 12
       Hash[banners.slice(0..1)]
