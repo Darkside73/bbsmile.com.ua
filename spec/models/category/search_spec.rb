5cf11f34a78c87042f86378b1a81888f529eb06b
darkside <andrey.garbuz@gmail.com>
2014-04-08 12:43:51 +0300

    Fix category search spec

--- spec/models/category/search_spec.rb
+++ spec/models/category/search_spec.rb
@@ -8,7 +8,7 @@ describe Category do
         products = category.products_grid(sort: :price, direction: 'desc')
         first_product_price  = products.first.variants.collect(&:price).max
         second_product_price = products.second.variants.collect(&:price).max
-        first_product_price.should be > second_product_price
+        first_product_price.should be >= second_product_price
       end
     end
     context "searching" do
