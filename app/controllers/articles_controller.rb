491c1c39bac24d65fad4f17d608a3c7f1e65591b
darkside <andrey.garbuz@gmail.com>
2014-07-18 16:58:10 +0300

    Fix articles controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,7 @@
 class ArticlesController < ApplicationController
 
   def show
-    @article = Article.visible.by_url params[:slug]
+    @article = Article.visible.by_url! params[:slug]
   end
 
   def index
