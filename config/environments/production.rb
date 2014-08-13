4b5a831a24ca1523eee5832a84267ea21bd6eaff
darkside <andrey.garbuz@gmail.com>
2014-08-13 23:29:50 +0300

    Disable starttls for action mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,6 +14,10 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
+  config.action_mailer.smtp_settings = {
+    enable_starttls_auto: false
+  }
+
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
   # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
