618d58ddd5af81d1873a1b05d1f2ea972d0d9890
darkside <andrey.garbuz@gmail.com>
2015-02-15 17:38:11 +0200

    Fix module inclusion in admin application helper

--- app/helpers/admin/application_helper.rb
+++ app/helpers/admin/application_helper.rb
@@ -1,25 +1,27 @@
-module Admin::ApplicationHelper
-  include ApplicationHelper
+module Admin
+  module ApplicationHelper
+    include ::ApplicationHelper
 
-  def render_form_for_category(category = nil, url= nil)
-    render partial: 'form', locals: { category: category.blank? ? @category : category, url: url }
-  end
-
-  def breadcrumb_items
-    items = []
-    if @product.present? && !@product.new_record?
-      items += @product.category.ancestors
-      items << @product.category
-      items << @product
-    elsif @category.present?
-      items += @category.ancestors
-      items << @category
+    def render_form_for_category(category = nil, url= nil)
+      render partial: 'form', locals: { category: category.blank? ? @category : category, url: url }
     end
-    items
-  end
 
-  def link_to_spreadsheet(sync_type, name, options={})
-    url = "https://spreadsheets.google.com/ccc?key=#{Settings.gdrive.docs[sync_type]}"
-    link_to name, url, options
+    def breadcrumb_items
+      items = []
+      if @product.present? && !@product.new_record?
+        items += @product.category.ancestors
+        items << @product.category
+        items << @product
+      elsif @category.present?
+        items += @category.ancestors
+        items << @category
+      end
+      items
+    end
+
+    def link_to_spreadsheet(sync_type, name, options={})
+      url = "https://spreadsheets.google.com/ccc?key=#{Settings.gdrive.docs[sync_type]}"
+      link_to name, url, options
+    end
   end
 end
