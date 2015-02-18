7ffa1f833f9497b4b7d2ed032931b3343dff3f04
darkside <andrey.garbuz@gmail.com>
2015-02-18 16:00:58 +0200

    Add job queue adapter option to application config

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,8 @@ module Bbsmile
 
     config.autoload_paths += %W(#{config.root}/lib)
 
+    config.active_job.queue_adapter = :delayed_job
+
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     config.time_zone = 'Kyiv'
