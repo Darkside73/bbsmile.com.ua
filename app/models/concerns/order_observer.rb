a6f0c61a5dde15b9461d1b591ac7cfb1c155f63a
darkside <andrey.garbuz@gmail.com>
2015-06-04 14:59:58 +0300

    Revert "Temporary disable SMS sending on order"
    This reverts commit 70a3b008f88154938bffa7a1618b6dd230d10d9e.

--- app/models/concerns/order_observer.rb
+++ app/models/concerns/order_observer.rb
@@ -10,5 +10,11 @@ module OrderObserver
   def send_messages
     OrderMailer.new_order(self).deliver_now if self.user.email.present?
     ManagerMailer.new_order(self).deliver_now
+    if self.phone_number
+      require "#{Rails.root}/app/jobs/sms_send_job"
+      Delayed::Job.enqueue SmsSendJob.new(
+        self.phone_number, I18n.t('mailers.order.new_order.sms', order_id: self.id)
+      )
+    end
   end
 end
