629629b7b38a11c9f6dd76a3764c4e30bc922579
darkside <andrey.garbuz@gmail.com>
2015-07-20 21:29:37 +0300

    Switch to activerecord session store

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150718195006) do
+ActiveRecord::Schema.define(version: 20150720161643) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -133,6 +133,16 @@ ActiveRecord::Schema.define(version: 20150718195006) do
   end
 
   add_index "related_pages", ["page_id"], name: "index_related_pages_on_page_id", using: :btree
+
+  create_table "sessions", force: :cascade do |t|
+    t.string   "session_id", null: false
+    t.text     "data"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "sessions", ["session_id"], name: "index_sessions_on_session_id", unique: true, using: :btree
+  add_index "sessions", ["updated_at"], name: "index_sessions_on_updated_at", using: :btree
 
   create_table "suborders", force: :cascade do |t|
     t.integer "order_id"
