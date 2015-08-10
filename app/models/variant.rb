065eae110d0ebba30a03e71070fcad38caf1ab9d
darkside <andrey.garbuz@gmail.com>
2015-08-10 11:46:00 +0300

    Fix undesirable product variant image deleting

--- app/models/variant.rb
+++ app/models/variant.rb
@@ -24,7 +24,9 @@ class Variant < ActiveRecord::Base
   validates :available, :master, inclusion: { in: [true, false] }
   validate :price_old_cannot_be_less_than_price
 
-  before_save :destroy_image, if: "delete_image"
+  normalize_attribute :delete_image, with: :booleanize
+
+  before_save :destroy_image, if: :delete_image
 
   def title
     [product.name, name, sku].reject(&:blank?).join(' ')
