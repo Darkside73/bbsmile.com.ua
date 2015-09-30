ac4e97a3dbf64121f950bdd63dbe93157c10cc84
darkside <andrey.garbuz@gmail.com>
2015-09-30 17:38:38 +0300

    Add new category page promotion

--- app/cells/promotions_cell.rb
+++ app/cells/promotions_cell.rb
@@ -2,7 +2,14 @@ class PromotionsCell < Cell::Rails
 
   def category(args)
     category = args[:category]
-    banners = {}
+    banners = {
+      'mebel.jpg' => page_path(
+        'duetyi-krovat-komod',
+        anchor: 'promo4', utm_campaign: 'duetyi-krovat-komod',
+        utm_medium: 'category_page_side',
+        utm_source: 'local'
+      )
+    }
     banners = banners.to_a.shuffle
     @banners = if category.id == 12
       Hash[banners.slice(0..1)]
