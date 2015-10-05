e2649a06c3cf6e83d8876b84248c895d33bc680c
darkside <andrey.garbuz@gmail.com>
2015-10-05 12:53:45 +0300

    Do not support html response for cart checkout

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,22 +5,15 @@ class OrdersController < ApplicationController
     cart.attributes = order_params
     respond_to do |format|
       if cart.save
-        flash_message = I18n.t(
+        flash.now[:success] = I18n.t(
           'flash.message.orders.created', order_id: cart.id
         )
         format.json do
-          flash.now[:success] = flash_message
           render json: cart.as_json.merge(flashes_in_json), status: :created
-          reset_cart
-        end
-        format.html do
-          flash[:success] = flash_message
-          redirect_to cart_checkout_path
           reset_cart
         end
       else
         format.json { render json: cart.errors, status: :unprocessable_entity }
-        format.html { render 'cart/checkout' }
       end
     end
   end
--- spec/controllers/orders_controller_spec.rb
+++ spec/controllers/orders_controller_spec.rb
@@ -7,27 +7,14 @@ describe OrdersController do
       session[:cart].suborders << build(:suborder)
       session[:cart].suborders << build(:suborder)
     end
-    context "when format json" do
-      it 'creates order' do
-        request.env["HTTP_ACCEPT"] = 'application/json'
-        xhr :post, :create, order: {
-          payment_method: :cash_to_courier,
-          user_attributes: attributes_for(:user)
-        }
-        expect(flash[:success]).to have_content(/created/i)
-        expect(session[:cart]).to be_nil
-      end
-    end
-    context "when format html" do
-      it 'creates order and do redirect' do
-        post :create, order: {
-          payment_method: :cash_to_courier,
-          user_attributes: attributes_for(:user)
-        }
-        expect(flash[:success]).to have_content(/created/i)
-        expect(response).to redirect_to(cart_checkout_path)
-        expect(session[:cart]).to be_nil
-      end
+    it 'creates order' do
+      request.env["HTTP_ACCEPT"] = 'application/json'
+      xhr :post, :create, order: {
+        payment_method: :cash_to_courier,
+        user_attributes: attributes_for(:user)
+      }
+      expect(flash[:success]).to have_content(/created/i)
+      expect(session[:cart]).to be_nil
     end
   end
 
