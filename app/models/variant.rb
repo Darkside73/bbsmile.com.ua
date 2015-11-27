b47c9d1ff709433272b4994b724d6749703cb0d0
darkside <andrey.garbuz@gmail.com>
2015-11-27 16:22:06 +0200

    Show available variants first

--- app/models/variant.rb
+++ app/models/variant.rb
@@ -12,7 +12,7 @@ class Variant < ActiveRecord::Base
   accepts_nested_attributes_for :image, reject_if: :all_blank
   acts_as_list scope: :product
 
-  default_scope -> { order(:position) }
+  default_scope -> { order(:position).order(available: :desc) }
   scope :available, -> { where(available: 1) }
   scope :discounts, -> {
     where(self.arel_table[:price_old].not_eq(nil)).
