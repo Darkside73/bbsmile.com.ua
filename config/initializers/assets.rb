b3e72605629ff676a72e0598b3aef98716f7e451
darkside <andrey.garbuz@gmail.com>
2016-02-13 19:32:42 +0200

    Fix assets precompile list for tinymce plugins

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,7 +6,7 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 Rails.application.config.assets.precompile += [
-  'tinymce/plugins/youtube/*.js', 'tinymce/plugins/youtube/*.css', 'tinymce/plugins/youtube/*.html'
+  'tinymce/*.js', 'tinymce/*.css', 'tinymce/plugins/youtube/*.html'
 ]
 Rails.application.config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif *.ttf *.eot *.svg *.woff 404.html 500.html)
 
