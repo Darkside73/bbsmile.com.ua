fd545c628a6fe2af8dad05564925750ed633d3a3
darkside <andrey.garbuz@gmail.com>
2016-03-04 12:35:16 +0200

    Fix rendering autocomplete search.
    In Rails 5 "object" option passed to render
    assigns variable named :"<object>.html"

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -6,13 +6,16 @@ class SearchController < ApplicationController
   def autocomplete
     results = []
     SiteSearch::Autocomplete.results_for(params[:q]).each do |item|
+      content = render_to_string(
+        locals: { item: item },
+        partial: "autocomplete/#{item.class.name.underscore}.html"
+      )
       results << {
         id:   item.id,
         type: item.class.name,
         name: item.title,
         url:  self.send("#{item.class.name.underscore}_page_path", item.url),
-        html: render_to_string(object: item,
-                               partial: "autocomplete/#{item.class.name.underscore}.html")
+        html: content
       }
     end
 
