611b4b475957efb8d8ea79b489a6a992ca17beed
darkside <andrey.garbuz@gmail.com>
2016-09-10 02:11:57 +0300

    Use paperclip-compression gem

--- app/models/article/image.rb
+++ app/models/article/image.rb
@@ -1,5 +1,5 @@
 class Article::Image < Asset
-  has_attached_file :attachment, url: DEFAULT_URL, path: DEFAULT_PATH
+  has_attached_file :attachment, processors: [:compression], url: DEFAULT_URL, path: DEFAULT_PATH
   validates_attachment_content_type :attachment, content_type: /^image\/(png|gif|jpg|jpeg)/,
                                     message: I18n.t('errors.messages.paperclip.content_type_image')
 
